fcb7dcbf72f6e915615ad23c4d8d77f9b17fb246
TakuoIshida <archersparadox.100@gmail.com>
2021-07-23 15:36:37 +0900

    iam role attach OK

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+module "describe_region_for_ec2" {
+  source     = "./iam_role/"
+  name       = "describe-region-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
